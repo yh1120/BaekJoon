7906d5b83fccbee64d1bdc412c3193d42e5854d0
yonglee <yonglee@student.42seoul.kr>
2020-12-11 12:22:09 +0900

    2231

--- 2231.c
+++ 2231.c
@@ -6,22 +6,24 @@ int		main(void)
 	int	arr[7] = {0, };
 	int k;
 
-	k = 0;
+
 	scanf("%d", &num);
 	for (int i = 1; i <= num; i++)
 	{
+		k = 0;
 		for (int j = i; j > 0; j = j / 10)
 		{
 			arr[k++] = j % 10;
 		}
 		if (i + arr[0] + arr[1] + arr[2] + arr[3] + arr[4] + arr[5] + arr[6] == num)
 		{
-			printf("\n%d\n", i);
-			break;
+			printf("%d", i);
+			return (0);
 		}
-//		for (int i = 0; i < 7; i++)
-//			arr[i] = 0;
+		for (int i = 0; i < 7; i++)
+			arr[i] = 0;
 	}
+	printf("0");
 	return (0);
 
 }
