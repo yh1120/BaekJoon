6d352cefae3315333818d69fee353bbbc322d5bd
yonglee <yonglee@student.42seoul.kr>
2020-12-11 14:04:34 +0900

    7568

---
+++ 7568.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int		main(void)
+{
+	int num;
+	int	weight[50] = {0, };
+	int	height[50] = {0, };
+	int	ret[50] = {1, };
+
+	scanf("%d", &num);
+	for (int i = 0; i < num; i++)
+	{
+		scanf("%d %d", &weight[i], &height[i]);
+		printf("%d %d", weight[i], height[i]);
+	}
+	for (int j = 0; j < num; j++)
+		for (int i = 0; i < num; i++)
+			if (weight[j] < weight[i] && height[j] < height[i])
+				ret[j] += 1;
+	for(int i = 0; i < num - 1; i++)
+		printf("%d ", ret[i]);
+	printf("%d", ret[num - 1]);
+	return (0);
+
+}
